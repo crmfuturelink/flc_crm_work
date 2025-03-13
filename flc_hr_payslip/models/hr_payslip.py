2edc64b1ada264c40146ec1c98e5cddf834f3bb0
FLC-Ranjan <crmdeveloper.futurelink@gmail.com>
2025-03-13 17:15:28 +0530

    [IMP]PDF Formate added Improved

--- flc_hr_payslip/models/hr_payslip.py
+++ flc_hr_payslip/models/hr_payslip.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import pytz
 
 
 class HrPayslip(models.Model):
@@ -15,6 +18,53 @@ class HrPayslip(models.Model):
     total_earned_wages = fields.Float(string="Total Earned Wages", compute="_compute_total_earned_wages", store=True)
     total_deductions = fields.Float(string="Total Deductions", compute="_compute_total_deductions", store=True)
     net_payable_wage = fields.Float(string="Net Payable Wage", compute="_compute_net_payable_wage", store=True)
+    # Additional fields for tracking attendance
+    present_days = fields.Float(string='Present Days')
+    ph_days = fields.Float(string='Public Holiday Days', compute='_compute_attendance_days')
+    pl_days = fields.Float(string='Paid Leave Days', compute='_compute_attendance_days')
+    overtime_hours = fields.Float(string='Overtime Hours', compute='_compute_attendance_days')
+    weekly_off_days = fields.Float(string='Weekly Off Days', compute='_compute_attendance_days')
+    total_payable_days = fields.Float(string='Total Payable Days', default='25')
+    #
+    # # Amount in words
+    # amount_in_words = fields.Char(string='Amount in Words', compute='_compute_amount_in_words')
+    @api.model
+    def get_current_datetime(self):
+        """It  Will print Current Date and Time with Timezone On PDF footer"""
+        # Get current time in UTC
+        utc_now = datetime.now(pytz.utc)
+
+        # Convert to desired timezone
+        local_tz = pytz.timezone(self.env.user.tz or 'UTC')
+        local_now = utc_now.astimezone(local_tz)
+
+        # Get timezone abbreviation
+        tz_abbr = local_now.strftime('%Z')
+
+        # Format date and time with timezone abbreviation
+        return local_now.strftime('%B %d, %Y %I:%M:%S %p') + ' ' + tz_abbr
+
+    @api.depends()
+    def _compute_attendance_days(self):
+        return True
+
+
+
+    @api.depends('present_days', 'ph_days', 'pl_days', 'weekly_off_days')
+    def _compute_total_payable_days(self):
+        for payslip in self:
+            payslip.total_payable_days = payslip.present_days + payslip.ph_days + payslip.pl_days + payslip.weekly_off_days
+
+    @api.depends('line_ids', 'line_ids.total')
+    def _compute_amount_in_words(self):
+        for payslip in self:
+            net_salary = 0.0
+            for line in payslip.line_ids:
+                if line.code == 'NET':
+                    net_salary = line.total
+                    break
+
+            payslip.amount_in_words = payslip.currency_id.amount_to_text(net_salary)
 
     @api.depends('total_earned_wages', 'total_deductions')
     def _compute_net_payable_wage(self):
